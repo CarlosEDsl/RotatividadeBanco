5c8c7fa9fd3d9c26b7709841bc0e029e5f16f7b5
GuiIfsp <marques.soares@aluno.ifsp.edu.br>
2025-05-16 23:53:17 +0000

    Atualizando commit anterior

--- TrabalhoBancoAnalise.py
+++ TrabalhoBancoAnalise.py
@@ -12,7 +12,38 @@ df = pd.read_csv(filename)
 
 df.info()
 
-#Verificando valores ausentes
+#Verificando valores ausentes e mostrando as linhas com valores ausentes
 
-df.isnull().sum()
-nulos = df.isnull().any(axis=1)
+print(df[df.isnull().any(axis=1)])
+
+#Este comando da dropb em todas as linhas com valores ausentes
+
+df = df.dropna()
+
+#Resolvemos dar drop em todos os valores ausentes para não prejudicar a analise, ja que são poucas linhas e os dados são importantes
+
+#Gerando os histogramas e boxplots com seaborn
+numeric_cols = ['CreditScore', 'Age', 'Tenure', 'Balance',
+  'NumOfProducts', 'HasCrCard', 'IsActiveMember',
+  'EstimatedSalary', 'Exited']
+
+for col in numeric_cols:
+  plt.figure(figsize=(8, 4))
+  sns.histplot(df[col], kde=True, bins=30, color='skyblue')
+  plt.title(f'Histograma - {col}')
+  plt.xlabel(col)
+  plt.ylabel('Frequência')
+  plt.grid(True)
+  plt.tight_layout()
+  plt.show()
+
+for col in numeric_cols:
+  plt.figure(figsize=(8, 4))
+  sns.boxplot(x=df[col], color='lightcoral')
+  plt.title(f'Boxplot - {col}')
+  plt.xlabel(col)
+  plt.grid(True)
+  plt.tight_layout()
+  plt.show()
+
+#PARA VER OS GRAFICOS, ABRA A ABA VNC DO REPLIT
